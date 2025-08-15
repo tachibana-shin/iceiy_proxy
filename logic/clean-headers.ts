6fafd92d4057a25f959e906cb8a2f5d74490fa3f
tachib.shin <tachib.shin@gmail.com>
2025-08-15 06:10:55 +0000

    remove user=agent

--- logic/clean-headers.ts
+++ logic/clean-headers.ts
@@ -2,6 +2,7 @@ export function cleanHeaders(headers: Record<string, string>) {
   delete headers["host"]
   delete headers["via"]
   delete headers["traceparent"]
+  delete headers["user-agent"]
 
   console.log(headers)
 
